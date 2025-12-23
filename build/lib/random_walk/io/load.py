398b70dbff2e0ab280ef65944ef94544c03b9cdc
sanjitakr <sanjita.kumar011@gmail.com>
2025-12-23 09:23:55 +0530

    Remove .egg-info and add to .gitignore

--- build/lib/random_walk/io/load.py
+++
@@ -1,9 +0,0 @@
-import h5py
-
-
-def load_hdf5(filename):
-    data = {}
-    with h5py.File(filename, "r") as f:
-        for key in f.keys():
-            data[key] = f[key][:]
-    return data
--- build/lib/random_walk/io/save.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import h5py
-
-
-def save_hdf5(filename, **arrays):
-    with h5py.File(filename, "w") as f:
-        for name, arr in arrays.items():
-            f.create_dataset(name, data=np.asarray(arr))
