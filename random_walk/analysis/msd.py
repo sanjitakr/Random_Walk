4a29cb2c421553963d82a96992085c62ffeab5ce
sanjitakr <sanjita.kumar011@gmail.com>
2025-12-23 10:00:12 +0530

    update

--- random_walk/analysis/msd.py
+++ random_walk/analysis/msd.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-
-
-
 def mean_squared_displacement(trajectory):
+    trajectory = np.asarray(trajectory, dtype=float)
+    if trajectory.ndim == 1:
+        trajectory = trajectory.reshape(-1, 1)
+
     r0 = trajectory[0]
     dr = trajectory - r0
     msd = np.sum(dr**2, axis=1)
